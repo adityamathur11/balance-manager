c26eaef067b7b5e0681675d034266b18427a52f5
adityamathur11 <aditya.mathur2012@vit.ac.in>
2017-09-03 14:36:01 +0530

    base structure added

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Aditya on 31-Aug-17.
+ */
+var express = require('express')
+    ,mongoose = require('mongoose')
+    ,config = require('config')
+    ,bodyParser = require('body-parser')
+    ,passport = require('passport');
+
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./APIs/authentication/Passport')(passport);
+
+const PORT = process.env.port || 1000;
+
+connectDB()
+    .on('error' , function () {
+        console.log('error while connecting DB');
+    })
+    .on('disconnect',connectDB)
+    .once('open' , function () {
+        app.listen(PORT , function () {
+            console.log('Server is running on port :' , PORT);
+        });
+    });
+
+
+function connectDB() {
+    var options = {
+        server : {
+            socketOptions : {
+                keepAlive: 1
+            }
+        }
+    };
+    return mongoose.connect(config.db , {}).connection;
+}
